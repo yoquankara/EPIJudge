c31cae18e6f82003fd854420608aff236f2e2233
yoquan <663197+yoquankara@users.noreply.github.com>
2020-12-30 00:29:20 +0900

    5.1 array buy sell stock once - improve

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -6,15 +6,12 @@ from test_framework import generic_test
 def buy_and_sell_stock_once(prices: List[float]) -> float:
     # TODO - you fill in here.
     ret = 0.0
-    buy, sell = 0, 1
-    while buy < len(prices)-1:
-        while sell < len(prices) and prices[sell] >= prices[buy]:
-            ret = max(ret, prices[sell] - prices[buy])
-            sell += 1
-        if sell == len(prices) -1:
-            break
-        else:
-            buy = sell
+    buy = 0
+    for i in range(len(prices)):
+        if prices[i] > prices[buy]:
+            ret = max(ret, prices[i] - prices[buy])
+        elif prices[i] < prices[buy]:
+            buy = i
     return ret
 
 
