d36f73fc0511c498a7baca5594411422174724eb
yoquan <663197+yoquankara@users.noreply.github.com>
2020-12-30 14:12:58 +0900

    6.2 string convert base

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
+import string
 
 
 def convert_base(num_as_string: str, b1: int, b2: int) -> str:
     # TODO - you fill in here.
-    return ''
+    sign, start = '', 0
+    if num_as_string[0] == '-':
+        sign = '-'
+        start = 1
+
+    # Convert from b1 base to decimal base
+    d = 0
+    for i in range(start, len(num_as_string)):
+        d = d * b1 + string.hexdigits.index(num_as_string[i].lower())
+
+    # Convert to b2 base
+    ret = []
+    while d:
+        ret.append(string.hexdigits[d % b2].upper())
+        d //= b2
+    if not ret:
+        return '0'
+    ret.append(sign)
+
+    return ''.join(reversed(ret))
 
 
 if __name__ == '__main__':
