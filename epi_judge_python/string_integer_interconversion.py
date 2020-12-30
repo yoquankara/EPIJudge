9b2d1a208ec44cbb093dd2770168202ec89ab97c
yoquan <663197+yoquankara@users.noreply.github.com>
2020-12-30 13:44:24 +0900

    6.1 string integer conversion

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -4,12 +4,36 @@ from test_framework.test_failure import TestFailure
 
 def int_to_string(x: int) -> str:
     # TODO - you fill in here.
-    return '0'
+    sign = ''
+    if x == 0:
+        return '0'
+    elif x < 0:
+        sign = "-"
+        x = abs(x)
+    ret = []
+    while x:
+        ret.append(chr(ord('0') + x % 10))
+        x //= 10
+
+    ret.append(sign)
+
+    return "".join(reversed(ret))
 
 
 def string_to_int(s: str) -> int:
     # TODO - you fill in here.
-    return 0
+    start = 0
+    sign = 1
+    if s[0] == '-':
+        sign = -1
+        start = 1
+    elif s[0] == '+':
+        start = 1
+
+    ret = 0
+    for i in range(start, len(s)):
+        ret = ret * 10 + ord(s[i]) - ord('0')
+    return ret * sign
 
 
 def wrapper(x, s):
--- problem_mapping.js
+++ problem_mapping.js
@@ -490,7 +490,7 @@ problem_mapping = {
                 "total": 15002
             },
             "Python: string_integer_interconversion.py": {
-                "passed": 0,
+                "passed": 15002,
                 "total": 15002
             }
         },
