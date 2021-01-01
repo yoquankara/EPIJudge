47959a598d23f88fd78b67d48e18f3937dc75add
yoquan <663197+yoquankara@users.noreply.github.com>
2021-01-01 13:22:15 +0900

    6.4 string replace and remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -7,7 +7,30 @@ from test_framework.test_utils import enable_executor_hook
 
 def replace_and_remove(size: int, s: List[str]) -> int:
     # TODO - you fill in here.
-    return 0
+    # Forward pass, remove 'b' and count 'a'
+    write_idx, a_cnt = 0, 0
+    for i in range(size):
+        if s[i] != 'b':
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == 'a':
+            a_cnt += 1
+
+    curr = write_idx - 1
+    write_idx += a_cnt - 1
+    final_size = write_idx + 1
+
+    # Backward pass, pre-compute
+    while curr >= 0:
+        if s[curr] == 'a':
+            s[write_idx], s[write_idx-1] = 'd', 'd'
+            write_idx -= 2
+        else:
+            s[write_idx] = s[curr]
+            write_idx -= 1
+        curr -= 1
+
+    return final_size
 
 
 @enable_executor_hook
