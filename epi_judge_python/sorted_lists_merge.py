6a5d61f7c34803ac53fd0e2dc071c3855a0642cb
yoquan <663197+yoquankara@users.noreply.github.com>
2021-01-02 12:19:00 +0900

    7.1 merge 2 sorted list - dummy head

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -7,7 +7,17 @@ from test_framework import generic_test
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
     # TODO - you fill in here.
-    return None
+    dummy_head = tail = ListNode()
+
+    while L1 and L2:
+        if L1.data < L2.data:
+            tail.next, L1 = L1, L1.next
+        else:
+            tail.next, L2 = L2, L2.next
+        tail = tail.next
+
+    tail.next = L1 or L2
+    return dummy_head.next
 
 
 if __name__ == '__main__':
