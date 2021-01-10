e8950ea2c91378d00ee6392e19d304a0ff1b9932
yoquan <663197+yoquankara@users.noreply.github.com>
2021-01-10 13:34:23 +0900

    8.6 binary tree level order

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -6,7 +6,33 @@ from test_framework import generic_test
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
     # TODO - you fill in here.
-    return []
+    ret = []
+    if tree is None:
+        return ret
+    curr_level = [tree]
+
+    # Shorter code but longer time
+    #while curr_level:
+    #    ret.append([n.data for n in curr_level])
+    #    curr_level = [
+    #        child for n in curr_level
+    #        for child in (n.left, n.right) if child
+    #    ]
+    #return ret
+
+    while curr_level:
+        tmp = []
+        next_level = []
+        for n in curr_level:
+            tmp.append(n.data)
+            if n.left is not None:
+                next_level.append(n.left)
+            if n.right is not None:
+                next_level.append(n.right)
+        ret.append(tmp)
+        curr_level = next_level
+
+    return ret
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -952,7 +952,7 @@ problem_mapping = {
                 "total": 3852
             },
             "Python: tree_level_order.py": {
-                "passed": 0,
+                "passed": 3852,
                 "total": 3852
             }
         },
