cab76f4e01fe63fdc896d65ba0ceabad7a53ed9e
yoquan <663197+yoquankara@users.noreply.github.com>
2021-01-10 15:14:22 +0900

    8.2 reverse Polish notation

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -3,7 +3,20 @@ from test_framework import generic_test
 
 def evaluate(expression: str) -> int:
     # TODO - you fill in here.
-    return 0
+    if not expression:
+        return 0
+    stack = []
+    ops = {'+': lambda y, x: x + y,
+           '-': lambda y, x: x - y,
+           '*': lambda y, x: x * y,
+           '/': lambda y, x: x // y}
+    for c in expression.split(','):
+        if c in ops:
+            stack.append(ops[c](stack.pop(), stack.pop()))
+        else:
+            stack.append(int(c))
+
+    return stack.pop()
 
 
 if __name__ == '__main__':
