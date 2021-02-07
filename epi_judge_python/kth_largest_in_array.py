578a625a6994c41c99f732783e5a5d146dcb8532
yoquan <663197+yoquankara@users.noreply.github.com>
2021-02-07 14:42:02 +0900

    11.8 find kth largest element in array in place

--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -1,3 +1,4 @@
+import random
 from typing import List
 
 from test_framework import generic_test
@@ -8,7 +9,32 @@ from test_framework import generic_test
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k: int, A: List[int]) -> int:
     # TODO - you fill in here.
-    return 0
+    if k > len(A):
+        raise ValueError("k is larger than list size")
+
+    def partition_around_pivot(left, right, pivot_idx) -> int:
+        """Find new pivot idx that A[left:new_pivot_dix] are all greater than pivot value,
+        A[new_pivot_idx+1, right] are all less than pivot value"""
+        pivot_value = A[pivot_idx]
+        new_pivot_idx = left
+        A[pivot_idx], A[right] = A[right], A[pivot_idx]
+        for i in range(left, right):
+            if A[i] > pivot_value:
+                A[new_pivot_idx], A[i] = A[i], A[new_pivot_idx]
+                new_pivot_idx += 1
+        A[new_pivot_idx], A[right] = A[right], A[new_pivot_idx]
+        return new_pivot_idx
+
+    left, right = 0, len(A) - 1
+    while left <= right:
+        pivot_idx = random.randint(left, right)
+        new_pivot_idx = partition_around_pivot(left, right, pivot_idx)
+        if new_pivot_idx == k - 1:
+            return A[new_pivot_idx]
+        elif new_pivot_idx < k - 1:
+            left = new_pivot_idx + 1
+        else:
+            right = new_pivot_idx - 1
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -1406,7 +1406,7 @@ problem_mapping = {
                 "total": 503
             },
             "Python: kth_largest_in_array.py": {
-                "passed": 0,
+                "passed": 503,
                 "total": 503
             }
         },
