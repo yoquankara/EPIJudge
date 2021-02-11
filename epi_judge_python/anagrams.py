d76a5650fc62715d21c74922d073bdd3e1ee438d
yoquan <663197+yoquankara@users.noreply.github.com>
2021-02-11 21:46:30 +0900

    12.0 find anagrams group

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -1,11 +1,30 @@
-from typing import List
+import collections
+from typing import List, DefaultDict, Tuple
 
 from test_framework import generic_test, test_utils
 
 
 def find_anagrams(dictionary: List[str]) -> List[List[str]]:
-    # TODO - you fill in here.
-    return []
+    # O(nmlogm)
+    signatures: DefaultDict[str, List[str]] = collections.defaultdict(list)
+    for s in dictionary:
+        signatures[''.join(sorted(s))].append(s)
+
+    # O(nm)
+    # signatures: DefaultDict[Tuple[int], List[str]] = collections.defaultdict(list)
+    # for s in dictionary:
+    #     k_u = [0] * 27
+    #     k_p = [0] * 26
+    #     for c in s:
+    #         if c == ' ':
+    #             k_u[-1] += 1
+    #         elif c == c.lower():
+    #             k_u[ord(c) - ord('a')] += 1
+    #         else:
+    #             k_p[ord(c) - ord('A')] += 1
+    #     signatures[tuple(k_u + k_p)].append(s)
+    return [group for group in signatures.values()
+            if len(group) > 1]
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -1450,7 +1450,7 @@ problem_mapping = {
                 "total": 9
             },
             "Python: anagrams.py": {
-                "passed": 0,
+                "passed": 9,
                 "total": 9
             }
         },
