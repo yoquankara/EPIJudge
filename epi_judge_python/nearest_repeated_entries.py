d12f48e49d1a1144dcdea80f64345cf73b824e7f
yoquan <663197+yoquankara@users.noreply.github.com>
2021-02-14 21:24:14 +0900

    12.5 nearest repeated entries

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,11 +1,17 @@
-from typing import List
+from typing import List, DefaultDict
+from collections import defaultdict
 
 from test_framework import generic_test
 
 
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    str_to_pos: DefaultDict[str, int] = defaultdict()
+    min_distance = float('inf')
+    for i, p in enumerate(paragraph):
+        if p in str_to_pos:
+            min_distance = min(min_distance, i - str_to_pos[p])
+        str_to_pos[p] = i
+    return min_distance if min_distance < float('inf') else -1
 
 
 if __name__ == '__main__':
